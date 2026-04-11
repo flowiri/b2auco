2f69782f08a69861ec16be3ae43096316dbc8b6e
flowiri <fairytypean@gmail.com>
2026-04-11 12:50:22 +0700

    resolve warnings

--- src/test/java/com/example/b2auco/burp/B2aucoExtensionTest.java
+++ src/test/java/com/example/b2auco/burp/B2aucoExtensionTest.java
@@ -20,7 +20,6 @@ import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
